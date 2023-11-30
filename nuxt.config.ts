15fec8d278d9d390c187531f329b4a3d1eac1ed8
Satoru <you@example.com>
2023-11-30 20:58:40 +0900

    feat: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    "@nuxt/content",
+    '@vueuse/nuxt',
+  ],
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+
+const items = [
+  {
+    title: "Blog (Nuxt Content and Netlify CMS)",
+    to: "blog"
+  },
+];
+
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+</script>
+<template>
+  <v-container>
+    <v-list>
+      <v-list-item class="mb-2" v-for="(item, key) in items" :key="key">
+        
+          <NuxtLink :to="item.to">
+            {{ item.title }}
+          </NuxtLink>
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
